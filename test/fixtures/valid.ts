b2cafd2c797233cd96e11dbe56639451d019060b
Tymoteusz Dzienniak <dzienniak.tymoteusz@gmail.com>
2021-11-12 13:32:59 +0100

    feat: add simple smoke tests

---
+++ test/fixtures/valid.ts
@@ -0,0 +1,3 @@
+const foo = async () => {
+  return await Promise.resolve('bar');
+};
---
+++ test/test.spec.ts
@@ -0,0 +1,27 @@
+import { test } from 'uvu';
+import { ESLint } from 'eslint';
+import expect from 'expect';
+
+const eslint = new ESLint({
+  useEslintrc: false,
+  overrideConfigFile: 'node.js',
+  overrideConfig: {
+    parserOptions: {
+      project: 'test/fixtures/tsconfig.json',
+    }
+  }
+});
+
+test('should show errors in invalid file', async () => {
+  const [results] = await eslint.lintFiles(['test/fixtures/invalid.ts']);
+
+  expect(results.messages).toHaveLength(2);
+});
+
+test('should not show errors in valid file', async () => {
+  const [results] = await eslint.lintFiles(['test/fixtures/valid.ts']);
+
+  expect(results.messages).toHaveLength(0);
+});
+
+test.run();
